39598229d638e8b826a53ac4afd176f29158a501
Pashka <Pashka@PASHKA-PC54>
2024-03-13 16:49:41 +0300

    Добавьте файлы проекта.

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 105);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button10.Location = new System.Drawing.Point(10, 181);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(405, 58);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Начать игру";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button2.Location = new System.Drawing.Point(149, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 105);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button3.Location = new System.Drawing.Point(284, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 105);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button4.Location = new System.Drawing.Point(284, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 105);
+            this.button4.TabIndex = 14;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button5.Location = new System.Drawing.Point(149, 158);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 105);
+            this.button5.TabIndex = 13;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button6.Location = new System.Drawing.Point(12, 158);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 105);
+            this.button6.TabIndex = 12;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button9.Location = new System.Drawing.Point(12, 269);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(131, 105);
+            this.button9.TabIndex = 15;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button8.Location = new System.Drawing.Point(149, 269);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(131, 105);
+            this.button8.TabIndex = 16;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button7.Location = new System.Drawing.Point(284, 269);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(131, 105);
+            this.button7.TabIndex = 17;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.FieldElemClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(427, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Enabled = false;
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.новаяИграToolStripMenuItem.Text = "Новая Игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(22, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Победы Игрока:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(251, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 25);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Победы Бота:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        int XCount = 0;
+        int OCount = 0;
+        Button[,] field;
+        string player = "X";
+        bool freespace = true;
+        string Bot = "O";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateField()
+        {
+            field = new Button[3, 3];
+
+            field[0, 0] = button1;
+            field[0, 1] = button2;
+            field[0, 2] = button3;
+
+            field[1,0] = button6;
+            field[1,1] = button5;
+            field[1,2] = button4;
+
+            field[2,0] = button9;
+            field[2,1] = button8;
+            field[2,2] = button7;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    field[i, j].Text = "";
+                    field[i, j].Enabled = true;
+                    field[i, j].Visible = true;
+                }
+            }
+        }
+        public bool Win()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i,0].Text == field[i,1].Text && field[i,1].Text == field[i,2].Text && field[i,0].Text != "")
+                {
+                    return true;
+                }
+                
+                
+            }
+            for (int i = 0; i < 3; i++)
+            {
+             if (field[0, i].Text == field[1, i].Text && field[1, i].Text == field[2, i].Text && field[0, i].Text != "")
+             {
+                return true;
+             }
+
+
+            }
+            if (field[0, 0].Text == field[1, 1].Text && field[1, 1].Text == field[2, 2].Text && field[0, 0].Text != "")
+            {
+                return true;
+            }
+            if (field[2, 0].Text == field[1, 1].Text && field[1, 1].Text == field[0, 2].Text && field[2, 0].Text != "")
+            {
+                return true;
+            }
+            return false;
+
+        }
+       private void BotTurn()
+       {
+            Random r = new Random();
+            int corner = 0;
+            int udlr = 0;
+            while(true)
+            {
+                corner = r.Next(0, 4);
+                udlr = r.Next(0, 4);
+
+                if (field[1, 1].Text == "")
+                {
+                    field[1, 1].Text = "O";
+                    break;
+                }
+                else if (corner == 0)
+                {
+                    if (field[0, 0].Text == "")
+                    {
+                        field[0, 0].Text = "O";
+                        break;
+                    }
+                }
+
+                else if (corner == 1)
+                {
+                    if (field[0, 2].Text == "")
+                    {
+                        field[0, 2].Text = "O";
+                        break;
+                    }
+                }
+
+                else if (corner == 2)
+                {
+                    if (field[2, 0].Text == "")
+                    {
+                        field[2, 0].Text = "O";
+                        break;
+                    }
+                }
+
+                else if (corner == 3)
+                {
+                    if (field[2, 2].Text == "")
+                    {       
+                        field[2, 2].Text = "O";
+                        break;
+                    }
+                }
+
+                if (udlr == 0)
+                {
+                    if (field[0, 1].Text == "")
+                    {
+                        field[0, 1].Text = "O";
+                        break;
+                    }
+                }
+                if (udlr == 1)
+                {
+                    if (field[1, 2].Text == "")
+                    {
+                        field[1, 2].Text = "O";
+                        break;
+                    }
+                }
+                if (udlr == 2)
+                {
+                    if (field[1, 0].Text == "")
+                    {
+                        field[1, 0].Text = "O";
+                        break;
+                    }
+                }
+                if (udlr == 3)
+                {
+                    if (field[2, 1].Text == "")
+                    {
+                        field[2, 1].Text = "O";
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i, 0].Text == field[i, 1].Text && field[i, 1].Text == field[i, 2].Text && field[i, 0].Text != "")
+                {
+                    player = "O";
+                }
+
+
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[0, i].Text == field[1, i].Text && field[1, i].Text == field[2, i].Text && field[0, i].Text != "")
+                {
+                    player = "O";
+                }
+
+
+            }
+            if (field[0, 0].Text == field[1, 1].Text && field[1, 1].Text == field[2, 2].Text && field[0, 0].Text != "")
+            {
+                player = "O";
+            }
+            if (field[2, 0].Text == field[1, 1].Text && field[1, 1].Text == field[0, 2].Text && field[2, 0].Text != "")
+            {
+                player = "O";
+            }
+            if (Win())
+            {
+                if (player == "X")
+                {
+                    XCount++;
+                    label1.Text = $"Победы Игрока:{XCount}";
+                    MessageBox.Show($"Выйграл {player}");
+                    freespace = false;
+                }
+                else if (player == "O")
+                {
+                    OCount++;
+                    label2.Text = $"Победы Бота:{OCount}";
+                    MessageBox.Show($"Выйграл {player}");
+                    freespace = false;
+
+                }
+            }
+            if (FreeSpace() == false)
+            {
+                MessageBox.Show("Ничья");
+                CreateField();
+            }
+        }
+
+        private bool FreeSpace()
+        {
+            if (field[0,0].Text == field[0,1].Text && field[0,1] == field[0,2] && field[1, 0].Text == field[1, 1].Text && field[1, 1] == field[1, 2] && field[2, 0].Text == field[2, 1].Text && field[2, 1].Text == field[2, 2].Text)
+            {
+                return true;
+            }
+            return true;
+        }
+        private void FieldElemClick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (((Button)sender).Equals(field[i, j]))
+                    {
+                        if (field[i, j].Text == "")
+                        {
+                            field[i, j].Text = player;
+                        }
+                        if (Win())
+                        {
+                            if (player == "X")
+                            {
+                                XCount++;
+                                label1.Text = $"Победы Игрока:{XCount}";
+                                MessageBox.Show($"Выйграл {player}");
+                                freespace = false;
+                            }
+                            else if (player == "O")
+                            {
+                                OCount++;
+                                label2.Text = $"Победы Бота:{OCount}";
+                                MessageBox.Show($"Выйграл {player}");
+                                freespace = false;
+
+                            }
+                        }
+
+
+                        if (player == "X")
+                        {
+                            if (freespace == true)
+                            {
+                                BotTurn();
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            player = Bot;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            CreateField();
+            button10.Visible = false;
+            новаяИграToolStripMenuItem.Enabled = true;
+            player = "X";
+
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateField();
+            player = "X";
+            freespace = true;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+    }
+}
